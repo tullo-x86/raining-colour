1be6676b835d0e03ee18ea20c1b7f2d112b65f3d
LuminousElement <d.tullemans@gmail.com>
2014-11-10 15:23:32 +1000

    Multiple strips

--- src/config.h
+++ src/config.h
@@ -3,7 +3,7 @@
 
 #include "pixeltypes.h"
 
-#define NUM_STRIPS 1
+#define NUM_STRIPS 5
 #define LEDS_PER_STRIP 32
 #define NUM_LEDS (NUM_STRIPS * LEDS_PER_STRIP)
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-// Luminate: generic lighting doohickey
+// Rain
 // Author: Daniel Tullemans / "LuminousElement"
 // Created 2014
 
@@ -17,11 +17,22 @@ int main()
 	memset(frameBuffer, 0, sizeof(struct CRGB) * NUM_LEDS);
 	FastLED.addLeds<NEOPIXEL, 10>(frameBuffer, NUM_LEDS);
 
-	SparksPattern pattern(frameBuffer, LEDS_PER_STRIP, 3, 10, 28, 128, 20);
+	SparksPattern patterns[NUM_STRIPS] =
+	{
+		SparksPattern(frameBuffer + (LEDS_PER_STRIP * 0), LEDS_PER_STRIP, 3, 18, 28, 128, 24),
+		SparksPattern(frameBuffer + (LEDS_PER_STRIP * 1), LEDS_PER_STRIP, 3,  9, 28, 128, 12),
+		SparksPattern(frameBuffer + (LEDS_PER_STRIP * 2), LEDS_PER_STRIP, 3, 12, 28, 128, 16),
+		SparksPattern(frameBuffer + (LEDS_PER_STRIP * 3), LEDS_PER_STRIP, 3, 18, 28, 128, 24),
+		SparksPattern(frameBuffer + (LEDS_PER_STRIP * 4), LEDS_PER_STRIP, 3, 10, 28, 128, 20)
+	};
 
     while(1) {
-    	pattern.Logic();
-    	pattern.Render();
+    	for (unsigned char i = 0; i < NUM_STRIPS; i++)
+    	{
+    		patterns[i].Logic();
+    		patterns[i].Render();
+    	}
+
     	FastLED.show();
         _delay_ms(35);
     }
